5df37aa32fb346ecdae43184bb33b05a45432525
Praneeth <praneeth.nns@asanify.com>
2025-12-26 00:51:26 +0530

    fix(literature-reviews): 🐛 Fix issue where search and staging states are reset when a literature review is edited and saved

--- app/(app)/literature-reviews/[id]/literature-review-actions.tsx
+++ app/(app)/literature-reviews/[id]/literature-review-actions.tsx
@@ -1,17 +1,25 @@
-"use client"
+"use client";
 
-import { EditLiteratureReviewDialog } from './edit-literature-review-dialog'
-import { DeleteLiteratureReviewDialog } from './delete-literature-review-dialog'
+import { EditLiteratureReviewDialog } from "./edit-literature-review-dialog";
+import { DeleteLiteratureReviewDialog } from "./delete-literature-review-dialog";
 
-export function LiteratureReviewActions({ literature }: { literature: any }) {
+export function LiteratureReviewActions({
+  literature,
+  onRefresh,
+}: {
+  literature: any;
+  onRefresh?: () => void;
+}) {
   return (
     <div className="flex gap-2">
-      <EditLiteratureReviewDialog literature={literature} />
-      <DeleteLiteratureReviewDialog 
-        literatureId={literature.id} 
+      <EditLiteratureReviewDialog
+        literature={literature}
+        onSuccess={onRefresh}
+      />
+      <DeleteLiteratureReviewDialog
+        literatureId={literature.id}
         literatureTitle={literature.title}
       />
     </div>
-  )
+  );
 }
-
